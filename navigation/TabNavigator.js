d78cd55b6d08466a1da7e91bb8874f246f87573a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-28 22:35:43 +0900

    Tabs,Styles

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -9,9 +9,17 @@ import TabBarIcon from "../components/TabBarIcon";
 import HomeScreen from "../screens/HomeScreen";
 import SettingsScreen from "../screens/SettingsScreen";
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen
-});
+const StackNavOptions = {
+  headerStyle: {},
+  headerTitleStyle: {}
+};
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen
+  },
+  { ...StackNavOptions }
+);
 
 HomeStack.navigationOptions = {
   tabBarLabel: "Home",
@@ -27,11 +35,35 @@ HomeStack.navigationOptions = {
   )
 };
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen
-});
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen
+  },
+  { ...StackNavOptions }
+);
 
-export default createBottomTabNavigator({
-  HomeStack,
-  SettingsStack
-});
+SettingsStack.navigationOptions = {
+  tabBarLabel: "Settings",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === "ios"
+          ? `ios-information-circle${focused ? "" : "-outline"}`
+          : "md-information-circle"
+      }
+    />
+  )
+};
+
+export default createBottomTabNavigator(
+  {
+    HomeStack,
+    SettingsStack
+  },
+  {
+    tabBarOptions: {
+      showLabel: false
+    }
+  }
+);
